a59977fd12ab121de110c06d9d5b7c0a83a0e50a
mgonzalz <mgonzgarc@myuax.com>
2023-02-13 20:46:58 +0100

    ej 7

--- ejercicio02/ejercicio02.py
+++ ejercicio02/ejercicio02.py
@@ -1,7 +1,8 @@
-a = 5
-b = -3
-suma = a + b
-producto = a * b
-orden = [a, b, suma, producto]
-orden.sort()
-print(orden)
+'''
+Dados dos números cualesquiera.
+
+Clasificarlos respecto a su suma y a su producto.
+
+Así, por ejemplo, dados a = -15 y b = 6, se obtiene a x b < a < a + b < b cuyos sus valores son, en orden: -90, -15, -9 y 6.
+'''
+
---
+++ ejercicio02/ejercicio023.py
@@ -0,0 +1,16 @@
+PSEUDOCODIGO #Clasificacion de numeros respecto a su suma y producto ascendente
+
+ordenar
+
+Entrada:
+    a, b, suma, producto: ENTERO
+
+Inicializacion:
+    a ← ??? ; b ← ???
+
+Realizacion:
+    suma ← a + b ; producto ← a * b
+    orden ← [a, b, suma, producto]
+
+
+fin ordenar
